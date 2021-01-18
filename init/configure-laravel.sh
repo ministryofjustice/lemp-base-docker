73465c30da2cc2d38edb3169ac649a3fc4adefb1
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2021-01-18 21:18:24 +0000

    Prevent artisan from running

--- init/configure-laravel.sh
+++ init/configure-laravel.sh
@@ -3,10 +3,10 @@
 # run
 # XDebug will automatically read config from XDEBUG_CONFIG, so all we need to do here is enable it.
 
-if [ -f artisan ]
-then
-	php artisan config:clear
-	php artisan cache:clear
-	php artisan migrate --force
-	php artisan queue:restart
-fi
+#if [ -f artisan ]
+#then
+#	php artisan config:clear
+#	php artisan cache:clear
+#	php artisan migrate --force
+#	php artisan queue:restart
+#fi
