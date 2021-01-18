659ba0c11a8807dbbf075839fcf93ff5ee9924eb
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2021-01-18 22:06:39 +0000

    Let artisan run

--- init/configure-laravel.sh
+++ init/configure-laravel.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-# run
-# XDebug will automatically read config from XDEBUG_CONFIG, so all we need to do here is enable it.
-
-#if [ -f artisan ]
-#then
-#	php artisan config:clear
-#	php artisan cache:clear
-#	php artisan migrate --force
-#	php artisan queue:restart
-#fi
+if [ -f artisan ]
+then
+	php artisan config:clear
+	php artisan cache:clear
+	php artisan migrate --force
+	php artisan queue:restart
+fi
